8b175f1c7d3d0908c87b8da42c05ff5cbe320d91
r_ferguson <r.ferguson.3@research.gla.ac.uk>
2025-07-18 10:02:00 +0100

    vectorisation

--- GP_func.py
+++ GP_func.py
@@ -1,43 +1,40 @@
 import numpy as np
-from numpy.linalg import inv
-from scipy.spatial.distance import cdist
+from numpy.linalg import solve,cholesky
 
-def GP(x_known, y_known, e_known,x_fit,lengths):
+def GP(x_known, y_known, e_known, x_fit, lengths, batch_size=10000):
 
     '''
     Perform GP regression to find a predicted mean and uncertainty for unknown datapoints
     '''
 
-    y_fit=np.array([])
-    e_fit=np.array([])
+    y_fit = []
+    e_fit = []
 
-    starting_val=10000
-    l=0
+    K = kernel_func(x_known, x_known, lengths) + np.diag(e_known**2)
+    L = cholesky(K)  
 
-    K = kernel_func(x_known, x_known,lengths) + e_known**2 * np.eye(len(e_known))
-    K_inv = inv(K)
+    alpha = solve(L.T, solve(L, y_known))
 
-    while l<len(x_fit.T):
-
-        l+=starting_val
-
-        if l>len(x_fit.T):
-            x_fit_temp=x_fit.T[l-starting_val:].T
-        else:
-            x_fit_temp=x_fit.T[l-starting_val:l].T
-
-
-        K_s = kernel_func(x_known,x_fit_temp,lengths)
-        K_ss = kernel_func(x_fit_temp,x_fit_temp,lengths) 
-            
-        mu_s = K_s.T.dot(K_inv).dot(y_known)
-
-        sigma_s = np.sqrt(abs(np.diag(K_ss - K_s.T.dot(K_inv).dot(K_s))))
-
-        y_fit=np.concatenate((y_fit,mu_s))
-        e_fit=np.concatenate((e_fit,sigma_s))
+    total_points = x_fit.shape[1]
     
-    return y_fit,e_fit
+    for start in range(0, total_points, batch_size):
+        end = min(start + batch_size, total_points)
+        x_batch = x_fit[:, start:end]
+
+        K_s = kernel_func(x_known, x_batch, lengths)
+
+        K_ss_diag=np.ones(x_batch.shape[1])
+
+        mu_s = K_s.T @ alpha
+
+        v = solve(L, K_s)
+        var_s = np.clip(K_ss_diag - np.sum(v**2, axis=0), 1e-12, None)
+        sigma_s = np.sqrt(var_s)
+
+        y_fit.append(mu_s)
+        e_fit.append(sigma_s)
+
+    return np.concatenate(y_fit), np.concatenate(e_fit)
 
 #########################################################################
 
@@ -47,11 +44,14 @@ def kernel_func(x1,x2,l):
     Calculates the RBF kernel between 2 sets of points with given length scales for each dimension
     '''
     
-    sq_norm=np.zeros((x1.shape[1],x2.shape[1]))
 
-    for i in range(x1.shape[0]):
-        sq_norm+=cdist(x1[i].reshape(-1,1),x2[i].reshape(-1,1),"sqeuclidean")/(l[i]**2)
+    x1_scaled=x1/l[:,None]
+    x2_scaled=x2/l[:,None]
 
-    rbf = np.exp(-0.5*sq_norm)
+    x1_sq = np.sum(x1_scaled**2, axis=0).reshape(-1, 1)
+    x2_sq = np.sum(x2_scaled**2, axis=0).reshape(1, -1)
 
-    return rbf
+    sq_dist = x1_sq + x2_sq - 2 * np.dot(x1_scaled.T, x2_scaled)
+    sq_dist = np.maximum(sq_dist, 0)  
+
+    return np.exp(-0.5*sq_dist)
